106746258785b33aceb51c7ca2fdfcb47fd3c1bb
Новиков Илья <nov.ilucha@yandex.ru>
2023-05-19 17:46:42 +0300

    CommentState готов для мультистрочных комментариев

--- states/CommentState.cpp
+++ states/CommentState.cpp
@@ -1,22 +1,14 @@
 #include "StartState.h"
-#include "CompareState.h"
 #include "CommentState.h"
-#include "OperatorStates.h"
-#include "KeywordStates.h"
-#include "StringState.h"
-#include "NumberState.h"
-#include "IdentifierState.h"
-#include "Token.h"
-#include "TokenBuilder.h"
 #include "Context.h"
 #include <cctype>
 
 void CommentOperatorState::handle(char simbol, LexerContext *context) {
     switch (simbol) {
-        case '/':
+//        case '/':
 //            context->getTokenBuilder()->setType(OPERATOR);
 //            context->setState(new CommentOperatorState);
-            break;
+//            break;
         case '*':
             context->setState(new CommentState);
             break;
