00d265d937dc4f3f77403243e1b5b5ea4fc01f66
Новиков Илья <nov.ilucha@yandex.ru>
2023-05-19 18:32:37 +0300

    прописал состояния ключевым словам

--- states/KeywordStates.cpp
+++ states/KeywordStates.cpp
@@ -3,6 +3,7 @@
 #include "TokenBuilder.h"
 #include "IdentifierState.h"
 #include "Context.h"
+#include <cctype>
 
 using std::isdigit;
 using std::isalpha;
@@ -13,15 +14,14 @@ void Else1State::handle(char simbol, LexerContext *context) {
             context->getTokenBuilder()->addValue(simbol);
             context->setState(new Else2State);
             break;
+        case '_':
+        case '-':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new IdentifierState);
+            break;
         default:
-            if (isdigit(simbol)) {
+            if (isdigit(simbol) || isalpha(simbol)) {
                 context->getTokenBuilder()->addValue(simbol);
-                throw std::runtime_error(&"ERROR with this simbol: " [ simbol]);
-                return;
-            }
-            if (isalpha(simbol)) {
-                context->getTokenBuilder()->addValue(simbol);
-                context->getTokenBuilder()->setType(IDENTIFIER);
                 context->setState(new IdentifierState);
                 return;
             }
@@ -33,5 +33,198 @@ void Else1State::handle(char simbol, LexerContext *context) {
             context->getState()->handle(simbol, context);
             break;
     }
+    return;
+}
+
+void Else2State::handle(char simbol, LexerContext *context) {
+    switch (simbol) {
+        case 's':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new Else3State);
+            break;
+        case '_':
+        case '-':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new IdentifierState);
+            break;
+        default:
+            if (isdigit(simbol) || isalpha(simbol)) {
+                context->getTokenBuilder()->addValue(simbol);
+                context->setState(new IdentifierState);
+                return;
+            }
+            context->getTokenBuilder()->setType(IDENTIFIER);
+            context->storeToken();
+            context->setState(new StartState);
+            context->getState()->handle(simbol, context);
+            break;
+    }
+    return;
+}
+
+void Else3State::handle(char simbol, LexerContext *context) {
+    switch (simbol) {
+        case 'e':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new Else4State);
+            break;
+        case '_':
+        case '-':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new IdentifierState);
+            break;
+        default:
+            if (isdigit(simbol) || isalpha(simbol)) {
+                context->getTokenBuilder()->addValue(simbol);
+                context->setState(new IdentifierState);
+                return;
+            }
+            context->getTokenBuilder()->setType(IDENTIFIER);
+            context->storeToken();
+            context->setState(new StartState);
+            context->getState()->handle(simbol, context);
+            break;
+    }
+    return;
+}
+
+void Else4State::handle(char simbol, LexerContext *context) {
+    if (isdigit(simbol) || isalpha(simbol) || simbol == '-' || simbol == '_') {
+        context->getTokenBuilder()->addValue(simbol);
+        context->setState(new IdentifierState);
+        return;
+    }
+    context->getTokenBuilder()->setType(KEYWORD);
+    context->storeToken();
+    context->setState(new StartState);
+    context->getState()->handle(simbol, context);
+    return;
+}
+
+void If1State::handle(char simbol, LexerContext *context) {
+    switch (simbol) {
+        case 'f':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new If2State);
+            break;
+        case '_':
+        case '-':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new IdentifierState);
+            break;
+        default:
+            if (isdigit(simbol) || isalpha(simbol)) {
+                context->getTokenBuilder()->addValue(simbol);
+                context->setState(new IdentifierState);
+                return;
+            }
+            context->getTokenBuilder()->setType(IDENTIFIER);
+            context->storeToken();
+            context->setState(new StartState);
+            context->getState()->handle(simbol, context);
+            break;
+    }
+    return;
+}
+
+void If2State::handle(char simbol, LexerContext *context) {
+    if (isdigit(simbol) || isalpha(simbol) || simbol == '-' || simbol == '_') {
+        context->getTokenBuilder()->addValue(simbol);
+        context->setState(new IdentifierState);
+        return;
+    }
+    context->getTokenBuilder()->setType(KEYWORD);
+    context->storeToken();
+    context->setState(new StartState);
+    context->getState()->handle(simbol, context);
+    return;
+}
+
+void Then1State::handle(char simbol, LexerContext *context) {
+    switch (simbol) {
+        case 'h':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new Then2State);
+            break;
+        case '_':
+        case '-':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new IdentifierState);
+            break;
+        default:
+            if (isdigit(simbol) || isalpha(simbol)) {
+                context->getTokenBuilder()->addValue(simbol);
+                context->setState(new IdentifierState);
+                return;
+            }
+            context->getTokenBuilder()->setType(IDENTIFIER);
+            context->storeToken();
+            context->setState(new StartState);
+            context->getState()->handle(simbol, context);
+            break;
+    }
+    return;
+}
+void Then2State::handle(char simbol, LexerContext *context) {
+    switch (simbol) {
+        case 'e':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new Then3State);
+            break;
+        case '_':
+        case '-':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new IdentifierState);
+            break;
+        default:
+            if (isdigit(simbol) || isalpha(simbol)) {
+                context->getTokenBuilder()->addValue(simbol);
+                context->setState(new IdentifierState);
+                return;
+            }
+            context->getTokenBuilder()->setType(IDENTIFIER);
+            context->storeToken();
+            context->setState(new StartState);
+            context->getState()->handle(simbol, context);
+            break;
+    }
+    return;
+}
+void Then3State::handle(char simbol, LexerContext *context) {
+    switch (simbol) {
+        case 'n':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new Then4State);
+            break;
+        case '_':
+        case '-':
+            context->getTokenBuilder()->addValue(simbol);
+            context->setState(new IdentifierState);
+            break;
+        default:
+            if (isdigit(simbol) || isalpha(simbol)) {
+                context->getTokenBuilder()->addValue(simbol);
+                context->setState(new IdentifierState);
+                return;
+            }
+            context->getTokenBuilder()->setType(IDENTIFIER);
+            context->storeToken();
+            context->setState(new StartState);
+            context->getState()->handle(simbol, context);
+            break;
+    }
+    return;
+}
+
+void Then4State::handle(char simbol, LexerContext *context) {
+    if (isdigit(simbol) || isalpha(simbol) || simbol == '-' || simbol == '_') {
+        context->getTokenBuilder()->addValue(simbol);
+        context->setState(new IdentifierState);
+        return;
+    }
+    context->getTokenBuilder()->setType(KEYWORD);
+    context->storeToken();
+    context->setState(new StartState);
+    context->getState()->handle(simbol, context);
     return;
 }
